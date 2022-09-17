158e42f0854c9601b8901ecab3deb22d1528110a
irfan <engr.irfandayan@gmail.com>
2022-09-17 19:03:49 +0500

    express basics

---
+++ projects/express-app/util/path.js
@@ -0,0 +1,6 @@
+const path = require('path');
+// const { maainModule } = require('process');
+
+module.exports = path.dirname(require.main.filename);
+
+// module.exports = path.dirname(process.mainModule.filename);
