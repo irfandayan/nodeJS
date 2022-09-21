196ef41ac596aa892a2196d3310ad6d3f29a3050
irfan <engr.irfandayan@gmail.com>
2022-09-21 16:15:14 +0500

    add template engine ejs, pug and hbs

--- projects/express-app/util/path.js
+++ projects/express-app/util/path.js
@@ -1,6 +1,3 @@
 const path = require('path');
-// const { maainModule } = require('process');
 
-module.exports = path.dirname(require.main.filename);
-
-// module.exports = path.dirname(process.mainModule.filename);
+module.exports = path.dirname(process.mainModule.filename);
